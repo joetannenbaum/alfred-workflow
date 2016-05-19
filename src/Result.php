2fef46b2f5368b8fb9793712b1b438163b2851bf
Joe Tannenbaum <joe@joe.codes>
2016-05-19 16:46:01 -0400

    doc blocks for result

--- src/Result.php
+++ src/Result.php
@@ -28,13 +28,11 @@ class Result
 
     protected $mods = [];
 
-    public function generateUid($str)
-    {
-        $this->uid = md5($str);
-
-        return $this;
-    }
-
+    /**
+     * @param bool $valid
+     *
+     * @return \Alfred\Workflows\Result
+     */
     protected function setValid($valid = true)
     {
         $this->valid = !!$valid;
@@ -42,6 +40,12 @@ class Result
         return $this;
     }
 
+    /**
+     * @param string $type (deafult|file|file:skipcheck)
+     * @param bool $verify_existence When used with $type 'file'
+     *
+     * @return \Alfred\Workflows\Result
+     */
     protected function setType($type, $verify_existence = true)
     {
         if (in_array($type, ['default', 'file', 'file:skipcheck'])) {
@@ -55,6 +59,12 @@ class Result
         return $this;
     }
 
+    /**
+     * @param string $path
+     * @param string|null $type (fileicon|filetype)
+     *
+     * @return \Alfred\Workflows\Result
+     */
     protected function setIcon($path, $type = null)
     {
         $this->icon = [
@@ -68,16 +78,31 @@ class Result
         return $this;
     }
 
+    /**
+     * @param string $path
+     *
+     * @return \Alfred\Workflows\Result
+     */
     protected function setFileiconIcon($path)
     {
         return $this->setIcon($path, 'fileicon');
     }
 
+    /**
+     * @param string $path
+     *
+     * @return \Alfred\Workflows\Result
+     */
     protected function setFiletypeIcon($path)
     {
         return $this->setIcon($path, 'filetype');
     }
 
+    /**
+     * @param string $subtitle
+     *
+     * @return \Alfred\Workflows\Result
+     */
     protected function setSubtitle($subtitle)
     {
         $this->subtitle = $subtitle;
@@ -85,6 +110,12 @@ class Result
         return $this;
     }
 
+    /**
+     * @param string $type (copy|largetype)
+     * @param string $text
+     *
+     * @return \Alfred\Workflows\Result
+     */
     protected function setText($type, $text)
     {
         if (!in_array($type, ['copy', 'largetype'])) {
@@ -96,16 +127,34 @@ class Result
         return $this;
     }
 
+    /**
+     * @param string $copy
+     *
+     * @return \Alfred\Workflows\Result
+     */
     protected function setCopy($copy)
     {
         return $this->setText('copy', $copy);
     }
 
+    /**
+     * @param string $largetype
+     *
+     * @return \Alfred\Workflows\Result
+     */
     protected function setLargetype($largetype)
     {
         return $this->setText('largetype', $largetype);
     }
 
+    /**
+     * @param string $mod (shift|fn|ctrl|alt|cmd)
+     * @param string $subtitle
+     * @param string $arg
+     * @param bool $valid
+     *
+     * @return \Alfred\Workflows\Result
+     */
     protected function setMod($mod, $subtitle, $arg, $valid = true)
     {
         if (!in_array($mod, ['shift', 'fn', 'ctrl', 'alt', 'cmd'])) {
@@ -117,31 +166,71 @@ class Result
         return $this;
     }
 
+    /**
+     * @param string $subtitle
+     * @param string $arg
+     * @param bool $valid
+     *
+     * @return \Alfred\Workflows\Result
+     */
     protected function setCmd($subtitle, $arg, $valid = true)
     {
         return $this->setMod('cmd', $subtitle, $arg, $valid);
     }
 
+    /**
+     * @param string $subtitle
+     * @param string $arg
+     * @param bool $valid
+     *
+     * @return \Alfred\Workflows\Result
+     */
     protected function setShift($subtitle, $arg, $valid = true)
     {
         return $this->setMod('shift', $subtitle, $arg, $valid);
     }
 
+    /**
+     * @param string $subtitle
+     * @param string $arg
+     * @param bool $valid
+     *
+     * @return \Alfred\Workflows\Result
+     */
     protected function setFn($subtitle, $arg, $valid = true)
     {
         return $this->setMod('fn', $subtitle, $arg, $valid);
     }
 
+    /**
+     * @param string $subtitle
+     * @param string $arg
+     * @param bool $valid
+     *
+     * @return \Alfred\Workflows\Result
+     */
     protected function setCtrl($subtitle, $arg, $valid = true)
     {
         return $this->setMod('ctrl', $subtitle, $arg, $valid);
     }
 
+    /**
+     * @param string $subtitle
+     * @param string $arg
+     * @param bool $valid
+     *
+     * @return \Alfred\Workflows\Result
+     */
     protected function setAlt($subtitle, $arg, $valid = true)
     {
         return $this->setMod('alt', $subtitle, $arg, $valid);
     }
 
+    /**
+     * Converts the results to an array structured for Alfred
+     *
+     * @return array
+     */
     public function toArray()
     {
         $attrs = [
